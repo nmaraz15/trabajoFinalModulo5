9617496c68638aee30a5271c9a129f8255c5754d
nmaraz15 <nataliamaraz@gmail.com>
2023-04-12 12:19:39 +0200

    first commit

---
+++ src/api/data.js
@@ -0,0 +1,89 @@
+export const data = [
+  {
+    id: '1',
+    title: 'Punta Corral',
+    location: 'Tumbaya',
+    rating: '4.9',
+    description: 'Punta Corral: el operativo comienza el miércoles 29 de marzo. Bajo el lema “Peregrinos por amor a María, ensancha tu tienda”, la imagen de la Virgen descenderá el 2 de abril, mientras que el ascenso será el 29 del mismo mes..',
+    images: [
+      require('../../assets/images/puntaCorral1.webp'),
+      require('../../assets/images/puntaCorral2.webp'),
+      require('../../assets/images/puntaCorral3.webp'),
+      require('../../assets/images/puntaCorral4.webp')
+    ],
+    price: '$1000/noche',
+    locationCoordinates: {
+      latitude: -23.7222,
+      longitude: -65.5
+    }
+  },
+  {
+    id: '2',
+    title: 'Bajada de los Diablos',
+    location: 'Uquía',
+    rating: '3.5',
+    description: 'La Bajada de los Diablos es un hecho cultural que acontece el día de desentierro de carnaval y se da justamente porque el mojón (altar de piedras) de "Los Alegres" se encuentra en lo alto de un cerro.',
+    images: [
+      require('../../assets/images/bajadaDiablos1.webp'),
+      require('../../assets/images/bajadaDiablos2.webp'),
+      require('../../assets/images/bajadaDiablos3.webp')
+    ],
+    price: '$500/noche',
+    locationCoordinates: {
+      latitude: -23.30610153805632,
+      longitude: -65.35946008582322
+    }
+  },
+  {
+    id: '3',
+    title: 'Fiesta Nacional de los estudiantes',
+    location: 'San Salvador de Jujuy',
+    rating: '4.2',
+    description: 'La celebración transcurre a finales del mes de septiembre y dura 10 días, dando lugar al cambio de estación. Por tal razón, se dice que es en esta época en la cual la provincia de Jujuy se transforma en la “Capital Nacional de la Juventud y la Primavera”',
+    images: [
+      require('../../assets/images/estudiantes1.webp'),
+      require('../../assets/images/estudiantes2.webp'),
+      require('../../assets/images/estudiantes3.webp')
+    ],
+    price: '$250/noche',
+    locationCoordinates: {
+      latitude: -24.183163215378347,
+      longitude: -65.3312979025704
+    }
+  },
+  {
+    id: '4',
+    title: 'Peregrinación a Rio Blanco',
+    location: 'Río Blanco',
+    rating: '5.0',
+    description: 'En celebración a las Honras a la Virgen, todos los años la Peregrinación a Río Blanco moviliza a miles de personas en la provincia. El lema es “Como los mártires, junto a María firmes en la Fe”, las misas son a las 7, 8:30, 10:30, 12 y por la tarde la Misa Central será a las 18.15 en la Catedral.',
+    images: [
+      require('../../assets/images/rioBlanco1.webp'),
+      require('../../assets/images/rioBlanco2.webp'),
+      require('../../assets/images/rioBlanco3.webp')
+    ],
+    price: '$400/noche',
+    locationCoordinates: {
+      latitude: -24.22530764948662,
+      longitude: -65.23652224563921
+    }
+  },
+  {
+    id: '5',
+    title: 'La flor más bella',
+    location: 'San Salvador de Jujuy',
+    rating: '3.8',
+    description: 'Feria organizada por la Municipalidad y la Asociación de Productores de Plantas Ornamentales de Jujuy',
+    images: [
+      require('../../assets/images/flores1.webp'),
+      require('../../assets/images/flores2.webp'),
+      require('../../assets/images/flores3.webp')
+    ],
+    price: '$300/noche',
+    locationCoordinates: {
+      latitude: -24.257251861382017,
+      longitude: -65.27074096299968
+    }
+  }
+  
+]
